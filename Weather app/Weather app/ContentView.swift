53a81f438947db51e79a5133640d4d08086a0618
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-03-30 22:22:22 +0200

    created staring weather app
    https://www.youtube.com/watch?v=HXoVSbwWUIk&ab_channel=SeanAllen

---
+++ Weather app/Weather app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather app
+//
+//  Created by Mykyta Brazhynskyy on 30/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather app/Weather app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather app
+//
+//  Created by Mykyta Brazhynskyy on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
