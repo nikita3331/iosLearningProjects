9f5580f0916ad7671668de650ab4de849b1f9d36
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-04-14 12:42:44 +0200

    finished objc project

--- xxyy/xxyy/ViewController.m
+++ xxyy/xxyy/ViewController.m
@@ -10,6 +10,9 @@
 @interface ViewController ()
 @property (nonatomic,strong) NSString *login;
 @property (nonatomic,strong) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *loginField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
 
 @end
 
@@ -18,12 +21,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.login=[[NSString alloc]initWithString:@"siema"];
-    self.password=[[NSString alloc]initWithString:@"siema"];
-    [self.login stringByAppendingString:@"Bob"];
-    [self.password stringByAppendingString:@"secure"];
-    NSLog(@"%@",self.login);
+    self.login=@"siema";
+    self.password=@"siema";
+    self.passwordField.secureTextEntry=YES;
+    self.notificationLabel.text=@"";
 }
+- (IBAction)buttonAction:(id)sender {
+    BOOL valLogin=[self.login isEqualToString:[self.loginField text]];
+    BOOL valPass=[self.password isEqualToString:[self.passwordField text]];
+    if (valLogin &&valPass){
+        self.notificationLabel.textColor=UIColor.greenColor;
+        self.notificationLabel.text=@"Success";
+    }
+    else{
+        self.notificationLabel.textColor=UIColor.redColor;
+        self.notificationLabel.text=@"Wrong login/password";
+    }
+}
+
 - (void ) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self.view endEditing:YES];
 }
